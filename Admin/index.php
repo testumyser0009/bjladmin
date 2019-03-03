25185a10ff4f4c82a421e8263c5e4f3aa01e2fd9
Your Name <you@example.com>
2019-03-03 21:57:42 +0800

    ss

--- Admin/index.php
+++ Admin/index.php
@@ -13,6 +13,6 @@
 
 // 定义应用目录
 error_reporting(E_ALL ^ E_NOTICE);ini_set('display_errors','On');
-define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', __DIR__ . '/./application/');
 // 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+require __DIR__ . '/./thinkphp/start.php';
