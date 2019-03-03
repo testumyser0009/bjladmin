c30a1cfe9f67f480ae2091b063a295927d84166e
Your Name <you@example.com>
2019-03-03 21:50:47 +0800

    xiugai

--- Admin/index.php
+++ Admin/index.php
@@ -12,7 +12,7 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-
-define('APP_PATH', __DIR__ . '/./application/');
+error_reporting(E_ALL ^ E_NOTICE);ini_set('display_errors','On');
+define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
-require __DIR__ . '/./thinkphp/start.php';
+require __DIR__ . '/../thinkphp/start.php';
